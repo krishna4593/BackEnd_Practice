ac66c2f2f20a68a403738ff9d2d47c4c7c061668
krishna4593 <kamalkrishna489@gmail.com>
2026-08-19 12:48:31 +0530

    day-3 crud practice

--- Day3-CRUD/server.js
+++ Day3-CRUD/server.js
@@ -1,21 +1,51 @@
-const express = require('express');
-
-const app = express();
+const express = require("express")
+const app = express()
 app.use(express.json())
-const users=[]
-//create
-app.post("/create", (req,res)=>{
+let users=[]
+//create\
+app.post("/create",(req,res)=>{
     let body = req.body;
     users.push(body);
-    res.send("users saved successfully");
+    res.send({success:true,
+        message:"users saved successfully",
+        users
+    })
 })
+
 //read
 app.get("/",(req,res)=>{
-    res.send(users)
+    res.send(users);
 })
 
+//update
+app.put("/update/:id",(req,res)=>{
+let {id}= req.params
+let {name, age }=req.body
+let updatedUsers = users.map((val)=>{
+   return val.id===id ? {...val, name,age} : val;
+})
+users= updatedUsers
+res.send({
+    success:true,
+    message:"user updated successfully",
+    updatedUsers
+})
+})
 
+//delete
+app.delete("/delete/:id",(req,res)=>{
+    let {id}= req.params
+  let deleteUser=  users.filter((val)=>{
+     return   val.id !==id
+    })
+ users = deleteUser
+ res.send({
+    success:true,
+    message:"user deleted succesfully",
+   deleteUser
+ })
+})
 
-app.listen(3000, ()=>{
-    console.log("server is running on port 3000")
+app.listen(3000,()=>{
+    console.log("serevr is running on port 3000")
 })
